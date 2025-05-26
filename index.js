50abd16bcec9efdaa2ca128ceaf69e121d5082fa
Daniel Muhia <muhiadan17@gmail.com>
2025-05-26 16:33:19 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,39 @@
+//add
+function add(a, b){
+    return a + b;
+}
+
+//subtract
+function subtract(a, b){
+    return a-b;
+}
+
+//multiply
+function multiply(a, b){
+    return a*b;
+}
+
+//divide
+function divide(a, b){
+    return a/b;
+}
+
+//increment
+function increment(a){
+    return ++a;
+}
+
+//decrement
+function decrement(a){
+    return --a;
+}
+
+//makeInt(string)
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+//preserveDecimal(string)
+function preserveDecimal(n){
+    return parseFloat(n);
+}
